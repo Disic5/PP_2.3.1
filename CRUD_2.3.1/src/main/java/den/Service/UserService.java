607834be9344d08b2ad295bb46989d4dadb5dcf1
Denis <svd.denis@mail.ru>
2022-03-31 11:27:41 +0300

    2.3.1

---
+++ CRUD_2.3.1/src/main/java/den/Service/UserService.java
@@ -0,0 +1,15 @@
+package den.Service;
+
+
+
+import den.model.User;
+
+import java.util.List;
+
+public interface UserService {
+        List<User> index();
+        User show(int id);
+        void save(User user);
+        void update(User updatedUser);
+        void deleteById(int id);
+}
